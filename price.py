f260565b3531ba90e5487a948dd59777ad46365d
AlexeyMalaxov85 <94935244+AlexeyMalaxov85@users.noreply.github.com>
2021-11-24 14:41:01 +0300

    Update price.py

--- price.py
+++ price.py
@@ -14,9 +14,16 @@ def get_sum(one, two, delimiter='&'):
 summ = get_sum('Learn', 'python').upper()
 print(summ)
 
+#Задание 2 Функции
+#Создайте в редакторе файл price.py
+#Создайте функцию format_price, которая принимает один аргумент price
+#Приведите price к целому числу (тип int)
+#Верните строку "Цена: ЧИСЛО руб."
+#Вызовите функцию, передав на вход 56.24 и положите результат в переменную
+#Выведите значение переменной с результатом на экран
 
 def format_price(price):
-    price = float(price)
+    price = int(price)
     return f'Цена: + {price} + руб.'
 
 a = format_price(56.24)
